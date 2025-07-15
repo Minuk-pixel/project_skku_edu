2bfeb7c2eb27dbc8c57f85b75b0b3048dcc3e2a9
Minuk-pixel <minxu2002@g.skku.edu>
2025-07-15 16:19:33 +0900

    0715

--- main_time.py
+++ main_time.py
@@ -3,11 +3,29 @@
 
 from utils.lane_detect_v3 import LaneDetectV3
 from utils.steering_fsm import SteeringFSM
+from utils.crosswalk import Crosswalk
 import time
+
+
+def change_left(detector):
+    detector.send_serial_command("A")  # 왼쪽 기울이기
+    time.sleep(2.5)
+    detector.send_serial_command("D")  # 중앙으로 복귀
+    time.sleep(2.5)
+    detector.send_serial_command("C")
+
+
+def change_right(detector):
+    detector.send_serial_command("D")
+    time.sleep(2.5)
+    detector.send_serial_command("A")
+    time.sleep(2.5)
+    detector.send_serial_command("C")
 
 
 if __name__ == "__main__":
     detector = LaneDetectV3()
+    crosswalk_detector = Crosswalk()
     fsm = SteeringFSM(
         k=0.1,
         velocity=0.25,
@@ -16,11 +34,19 @@ if __name__ == "__main__":
     detector.send_serial_command("C")
 
     print("[INFO] Time trial mode started. Press Ctrl+C to stop.")
+    detector.current_target = "RIGHT"
 
     try:
         while True:
             result = detector.detect_lane_and_steering()
+            is_crosswalk = crosswalk_detector.crosswalk()
+            crosswalk_cooldown = 0
+
             if result is None:
+                continue
+
+            if crosswalk_cooldown > 0:
+                crosswalk_cooldown -= 1
                 continue
 
             cte = result['cte']
@@ -30,8 +56,20 @@ if __name__ == "__main__":
             detector.send_serial_command(f"T{angle:.2f}\n")
             detector.send_serial_command("F")
 
-            # detector.cam.arduino.write(f"T{angle:.2f}\n".encode())
-            # detector.cam.arduino.write(b"F")
+            if is_crosswalk and detector.current_target == "RIGHT":
+                print("✅ 횡단보도 감지됨!")
+                detector.set_lane("LEFT")
+                change_left(detector)
+                crosswalk_cooldown = 100  # 약 1초 정도 대기 (100 x 0.01s 루프 딜레이)
+                print("차선변경 완료!")
+                continue
+            elif is_crosswalk and detector.current_target == "LEFT":
+                print("✅ 횡단보도 감지됨!")
+                detector.set_lane("RIGHT")
+                change_right(detector)
+                crosswalk_cooldown = 100  # 약 1초 정도 대기 (100 x 0.01s 루프 딜레이)
+                print("차선변경 완료!")
+                continue
 
             time.sleep(0.01)
 
